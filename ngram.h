cc611e12e2b0afbaf59f6f562664619a80054e37
nursekti <59988090+nursekti@users.noreply.github.com>
2020-04-11 18:39:53 +0700

    Update ngram.h

--- ngram.h
+++ ngram.h
@@ -1,3 +1,12 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 9 – TUGAS BESAR
+* Kelompok : D-2
+* Hari dan Tanggal : Sabtu, 11 April 2020
+* Asisten (NIM) : Irfan Tito Kurniawan (18317019)
+* Nama File : ngram.h
+* Deskripsi : membuat tabel ngram
+*/
+
 void ngram(node *S, int count, tabelngram *tabel, int n){
     int i, j;
     char help[20];
