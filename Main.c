d2e95c2bc307ea131490efef549024bb6388a538
klmunifah <62830546+klmunifah@users.noreply.github.com>
2020-04-11 19:26:40 +0700

    Add files via upload

---
+++ Main.c
@@ -0,0 +1,62 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 8 – TUGAS BESAR
+* Kelompok : D-2
+* Hari dan Tanggal : Sabtu, 11 April 2020
+* Asisten (NIM) : Irfan Tito Kurniawan (18317019)
+* Nama File : main.c
+* Deskripsi : N-gram
+*/
+
+
+
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include "Ngram.h"
+#include "Find+AmbilNword.h"
+#include "pushpopprint.h"
+#include "bacacari.h"
+#include "randomcetak.h"
+
+typedef struct tubes{
+    char kata[20];
+    struct tubes *next;
+} node;
+
+typedef struct{
+    char *key;
+    char value[20];
+} tabelngram;
+
+int main(){
+    FILE *fileku;
+    node *isifile;
+    char filename[50];
+    int n, x, count;
+    tabelngram *tabel;
+    isifile = NULL;
+    count = 0;
+
+    printf("Masukkan nama file: ");
+    scanf("%s", &filename);
+    fileku = fopen(filename, "r");
+
+    while(strcmp(filename, "xxx")!=0){
+        bacafile(fileku, &isifile, &count);
+        cetaklist(isifile);
+        printf("%d\n", count);
+        printf("Masukkan nilai n untuk n-gram: ");
+        scanf("%d", &n);
+
+        do{
+            ngram(isifile, count, tabel, n);
+            printf("Masukkan jumlah kata yang ingin dicetak: ");
+            scanf("%d", &x);
+            cetakkata(tabel, x, count);
+        } while(x!=-99);
+
+        printf("Masukkan nama file: ");
+        scanf("%s", &filename);
+        fileku = fopen(filename, "r");
+    }
+}
