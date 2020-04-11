cb8bc9870f065d766bb857425ec4956a201ccea9
edoariasena <49637330+edoariasena@users.noreply.github.com>
2020-04-11 18:43:57 +0700

    Update and rename Searching + Ambil N word.h to Find+AmbilNword.h

--- Find+AmbilNword.h
+++ Find+AmbilNword.h
@@ -3,8 +3,8 @@
 * Kelompok : D-2
 * Hari dan Tanggal : Sabtu, 11 April 2020
 * Asisten (NIM) : Irfan Tito (18317019)
-* Nama File : Searching + Ambil N word.h
-* Deskripsi : Library ini berisi dua fungsi, yang pertama adalah searching / findword yang digunakan untuk mencari indeks dari key yang sesuai dengan n kata 
+* Nama File : Find+AmbilNword.h
+* Deskripsi : Library ini berisi dua fungsi, yang pertama adalah findword yang digunakan untuk mencari indeks dari key yang sesuai dengan n kata 
 * yang dimasukkan. N kata tersebut terlebih dahulu diambil dari susunan kata dari keluaran yang sedang di generate.
 * Sementara itu fungsi kedua adalah ambil N word yang digunakan untuk mengambil N word kata dari keluaran yang sedang di generate untuk 
 * dapat dimasukkan ke dalam fungsi findword.
