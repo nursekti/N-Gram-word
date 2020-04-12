d571a979a59a75ca9d4a48d19cd33ae7eaa52f35
edoariasena <49637330+edoariasena@users.noreply.github.com>
2020-04-12 18:18:14 +0700

    Update Find+AmbilNword.h

--- Find+AmbilNword.h
+++ Find+AmbilNword.h
@@ -1,7 +1,7 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
 * MODUL 9 – TUGAS BESAR - Soal 2 Ngram
 * Kelompok : D-2
-* Hari dan Tanggal : Sabtu, 11 April 2020
+* Hari dan Tanggal : Minggu, 12 April 2020
 * Asisten (NIM) : Irfan Tito Kurniawan (18317019)
 * Nama File : Find+AmbilNword.h
 * Deskripsi : Library ini berisi dua fungsi, yang pertama adalah findword yang digunakan untuk mencari indeks dari key yang sesuai dengan n kata 
@@ -13,53 +13,55 @@
 int findword(char *nword, tabelngram *tabel, int panjang)
 {
     int i = 0;
-    len = strlen(nword);
-    //tabel = (tabelngram*)malloc(panjang*sizeof(tabelngram));
-    //nword = (char*)malloc(len*sizeof(char));
-
-    while (i <= panjang || (strncmp(nword, tabel[i].key, len) != 0)
+    while (i <= panjang)
     {
-        return 0;
-        i = i + 1;
+        if (strcmp(nword, tabel[i].key) == 0)
+        {
+            return i;
+        } 
+        else
+        {
+            i = i + 1;
+        }
     }
-    return i;
+    return 0;
 }
 
-char* AmbilNWord(char* Hasil, int n) //Ambil n word terakhir untuk sekti
+void AmbilNWord(char* Hasil[], int n)
 {
     int count = 0;
     int k = 0;
     int j = strlen(Hasil);
 
-    Hasil = (char*)malloc(len*sizeof(char));
     char* temp = (char*)malloc(len*sizeof(char));
     char* Key = (char*)malloc(len*sizeof(char));
 
-    while((j >= 0) || (count != n))
+    while((j >= 0) && (count != n))
     {
-        if(Hasil[j] == ' ')
+        if(Hasil[j - 1] == ' ')
         {
+            count = count + 1;
             if (count < n)
             {
-                strcpy(temp[k]," ");
+                temp[k] = ' ';
                 k = k + 1;
             }
-            count = count + 1;
         }
         else
         {
-            strcpy(temp[k], Hasil[j]);
+            temp[k] = (*Hasil)[j - 1];
             k = k + 1;
         }
         j = j - 1;
     }
 
     int l = 0;
-    while(k >= 0)
+    while(k > 0)
     {
-        strcpy(Key[l], temp[k];
+        Key[l] = temp[k - 1];
         l = l + 1;
         k = k - 1;
     }
+    strcpy((*Hasil), Key);
 }
 
