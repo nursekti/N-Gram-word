8b109f5623e14271d7c09b37e78a68c2f341b778
nursekti <59988090+nursekti@users.noreply.github.com>
2020-04-13 09:53:49 +0700

    Update ngram.h

--- ngram.h
+++ ngram.h
@@ -1,35 +1,50 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
 * MODUL 9 – TUGAS BESAR
 * Kelompok : D-2
-* Hari dan Tanggal : Sabtu, 11 April 2020
+* Hari dan Tanggal : Senin, 13 April 2020
 * Asisten (NIM) : Irfan Tito Kurniawan (18317019)
 * Nama File : ngram.h
 * Deskripsi : membuat tabel ngram
 */
 
-void ngram(node *S, int count, tabelngram *tabel, int n){
-    int i, j;
-    char help[20];
+void ngram(node *S, long long int count, tabelngram **tabel, int n, long long int *row){
+    int i, j, k, x;
+    char help[20], *bantu;
     node *temp;
-    tabel = (tabelngram*)malloc(count*sizeof(tabelngram));
+    *tabel = (tabelngram*)malloc((count)*sizeof(tabelngram));
+    bantu = (char*)malloc((count*10)*sizeof(char));
     for(i=0;i<count;i++){
-        tabel[i].key = (char*)malloc(sizeof(char));
+        (*tabel)[i].key = (char*)malloc((count*15)*sizeof(char));
+        for(j=0; j<20; j++){
+            (*tabel)[i].value[j] = (char*)malloc(15*sizeof(char));
+            strcpy((*tabel)[i].value[j], " ");
+        }
     }
     temp = (node*)malloc(sizeof(node));
     temp = S;
-    printf("%s", help);
-    carilist(temp, (8%count), help);
-    printf("%s", help);
     for(i=0; i<count; i++){
         carilist(temp, i, help);
-        strcpy(tabel[i].key, help);
-        strcat(tabel[i].key, " ");
+        strcpy(bantu, help);
+        strcat(bantu, " ");
         for(j=1; j<n; j++){
             carilist(temp, ((i+j)%count), help);
-            strcat(tabel[i].key, help);
-            strcat(tabel[i].key, " ");
+            strcat(bantu, help);
+            strcat(bantu, " ");
         }
+        x = findwords(bantu, *tabel, *row);
+        j = 0;
         carilist(temp, ((i+n)%count), help);
-        strcpy(tabel[i].value, help);
+        if(x==0){
+            strcpy((*tabel)[*row].key, bantu);
+            strcpy((*tabel)[*row].value[0], help);
+            (*row)++;
+        } else{
+            while((strcmp((*tabel)[x].value[j], " ") != 0)&&((strcmp((*tabel)[x].value[j], help) != 0))){
+                j++;
+            }
+            strcpy((*tabel)[x].value[j], help);
+        }
+        pop(temp);
+        printf("%d\n", i);
     }
 }
