a439f0018db8424c4e96b08b43f431e97d2f7eb6
edoariasena <49637330+edoariasena@users.noreply.github.com>
2020-04-15 23:54:41 +0700

    Update List.h

--- List.h
+++ List.h
@@ -5,11 +5,10 @@
 * Asisten (NIM) : Irfan Tito Kurniawan (18317019)
 * Nama File : List.h
 * Deskripsi : Bagian ini berisi fungsi untuk memproses Linked List
-* Push berfungsi untuk Mempush Linked List
-* CariList berfunngsi untuk mencari List
+* Push berfungsi untuk menge-push tiap string dari file masukan ke sebuah Linked List
+* CariList berfungsi untuk mencari kata pada List untuk dimasukkan ke Key dari tabel NGram
 */
 
-//Push berfungsi untuk mempush linked list dengan input linked list S dan char c[20]
 node* Push(node* S, char c[20]){
     node *temp = (node*)malloc(sizeof(node));
     node *iter;
@@ -27,8 +26,6 @@ node* Push(node* S, char c[20]){
     return S;
 }
 
-
-//CariList berfungsi untuk mencari bagian dari linked list S dengan panjang n yang memiliki isi char x[20]
 void CariList(node *S, int n, char x[20]){
     node *temp;
     int i;
