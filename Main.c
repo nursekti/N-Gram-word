516fb81045c85de170c71af622f5a0c0bf93f58b
klmunifah <62830546+klmunifah@users.noreply.github.com>
2020-04-15 20:49:37 +0700

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,10 +1,10 @@
 /* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
 * MODUL 8 – TUGAS BESAR
 * Kelompok : D-2
-* Hari dan Tanggal : Sabtu, 11 April 2020
+* Hari dan Tanggal : Sabtu, 15 April 2020
 * Asisten (NIM) : Irfan Tito Kurniawan (18317019)
 * Nama File : main.c
-* Deskripsi : N-gram
+* Deskripsi : Program Utama
 */
 
 
@@ -12,52 +12,69 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
-#include "ngram.h"
-#include "Find+AmbilNword.h"
-#include "pushpopprint.h"
-#include "bacafile&carilist.h"
-#include "randomcetak.h"
-#include "Nvalue.h"
+#include<time.h>
 
 typedef struct tubes{
-    char kata[20];
+    char kata[100];
     struct tubes *next;
 } node;
 
 typedef struct{
     char *key;
-    char value[20];
+    char *value[20];
 } tabelngram;
+
+#include "List.h"
+#include "Input.h"
+#include "Proses.h"
+#include "Output.h"
+
+void Interface() {
+    printf("Selamat Datang di Program N-Gram Processing\n");
+    printf("Petunjuk penggunaa: \n");
+    printf("1. Masukkan nama file dengan benar sesuai yang ada dalam folder penyimpanan program\n");
+    printf("2. Masukkan nilai n yang digunakan untuk N-Gram Processing\n");
+    printf("3. Masukkan jumlah kata yang ingin dicetak \n");
+    printf("4. Jika Anda mengisi nilai -99 pada kolom jumlah kata yang ingin dicetak, \n");
+    printf("   program akan kembali ke interface\n");
+    printf("5. Untuk mengakhiri program masukkan input xxx pada masukan nama file \n");
+    printf("----------------------------------------------------------------------------------------\n");
+    return;}
 
 int main(){
     FILE *fileku;
     node *isifile;
     char filename[50];
-    int n, x, count;
+    int n, x, count, row, nhuruf;
     tabelngram *tabel;
-    isifile = NULL;
-    count = 0;
-
+    system("cls");
+    Interface();
     printf("Masukkan nama file: ");
     scanf("%s", &filename);
     fileku = fopen(filename, "r");
-
+    system("cls");
     while(strcmp(filename, "xxx")!=0){
-        bacafile(fileku, &isifile, &count);
-        cetaklist(isifile);
-        printf("%d\n", count);
+        isifile = NULL;
+        count = 0;
+        row = 0;
+        BacaFile(fileku, &isifile, &count);
         printf("Masukkan nilai n untuk n-gram: ");
         scanf("%d", &n);
-
-        do{
-            ngram(isifile, count, tabel, n);
-            printf("Masukkan jumlah kata yang ingin dicetak: ");
+        NGram(isifile, count, &tabel, n, &row);
+        printf("Masukkan jumlah kata yang ingin dicetak: ");
+        scanf("%d", &x);
+        while(x!=-99){
+            CetakKata(tabel, n, x, row);
+            printf("\n\n");
+            printf("Masukkan jumlah kata yang ingin dicetak (-99 untuk kembali ke interface): ");
             scanf("%d", &x);
-            cetakkata(tabel,n, x, count);
-        } while(x!=-99);
-
+        }
+        system("cls");
+        Interface();
         printf("Masukkan nama file: ");
         scanf("%s", &filename);
         fileku = fopen(filename, "r");
+        system("cls");
     }
+    return 0;
 }
