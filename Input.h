8fd57e8e1e92e92aac66d716e751e376ad80b00f
edoariasena <49637330+edoariasena@users.noreply.github.com>
2020-04-15 23:17:38 +0700

    Update Input.h

--- Input.h
+++ Input.h
@@ -4,15 +4,15 @@
 * Hari dan Tanggal : Rabu, 15 April 2020
 * Asisten (NIM) : Irfan Tito Kurniawan (18317019)
 * Nama File : Input.h
-* Deskripsi : Membaca file eksternal (.txt) yang diinput oleh program utama supaya dapat
-*             dimasukkan ke dalam tabel NGram.
+* Deskripsi : Membaca file eksternal (.txt) yang diinput oleh program utama dan memasukkannya ke dalam Linked-List
+*             sebelum akhirnya dimasukkan ke dalam tabel NGram (array) oleh fungsi NGram().
 */
 
 void BacaFile(FILE *S, node **x, int *count){
     char temp[100];
-    while(!feof(S)){
-        fscanf(S, "%s", temp);
-        *x = Push(*x, temp);
-        (*count)++;
+    while(!feof(S)){                    //While loop sampai mencapai end of file (feof)
+        fscanf(S, "%s", temp);          //Memasukkan file ke dalam array temp (unruk wadah sementara) tiap stringnya
+        *x = Push(*x, temp);            //Menge-push string dalam file temp tadi ke suatu linked-list x
+        (*count)++;                     //Program juga menghitung banyaknya string yang ada untuk mencari tahu ukuran banyaknya string dalam file
     }
 }
