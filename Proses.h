9c6eaf777628b1595ad3fca09fd3e11dec7d59c8
nursekti <59988090+nursekti@users.noreply.github.com>
2020-04-16 00:00:32 +0700

    Update Proses.h

--- Proses.h
+++ Proses.h
@@ -31,34 +31,34 @@ void NGram(node *S, int count, tabelngram **tabel, int n, int *row){
     node *temp;
     *tabel = (tabelngram*)malloc(count*sizeof(tabelngram));
     bantu = (char*)malloc((n*50)*sizeof(char));
-    for(i = 0; i < count; i++){                                             //For loop sampai akhir array
+    for(i = 0; i < count; i++){                                       //mengalokasi memori key dan value pada tabel
         (*tabel)[i].key = (char*)malloc((n*50)*sizeof(char));
         for(j = 0; j < 20; j++){
             (*tabel)[i].value[j] = (char*)malloc(15*sizeof(char));
-            strcpy((*tabel)[i].value[j], " ");
+            strcpy((*tabel)[i].value[j], " ");                      //menginiasialisasi semua value dengan " "
         }
     }
     temp = (node*)malloc(sizeof(node));
     temp = S;
     for(i = 0; i < count; i++){
-        CariList(temp, i, help);
-        strcpy(bantu, help);
+        CariList(temp, i, help);            //mancari kata urutan ke-i dari list temp, lalu disimpan ke variabel help
+        strcpy(bantu, help);                //mengkopi string pada help ke variabel bantu
         strcat(bantu, " ");
-        for(j = 1; j < n; j++){
+        for(j = 1; j < n; j++){             //mengulangi pembacaan list dan disimpan ke temp hingga n kata tersimpan
             CariList(temp, ((i+j)%count), help);
             strcat(bantu, help);
             strcat(bantu, " ");
         }
-         x = CariWords(bantu, *tabel, *row);
+         x = CariWords(bantu, *tabel, *row);    //mengecek apakah sudah ada string bantu pada key, jika ada, x merujuk ke indeks tersebut
         j = 0;
-        CariList(temp, ((i+n)%count), help);
-        if(x == 0){
+        CariList(temp, ((i+n)%count), help);    //mancari kata urutan ke-((i+n)%count) dari list temp, lalu disimpan ke variabel help
+        if(x == 0){                             //jika tidak ada string bantu yang tersimpan pada key
             strcpy((*tabel)[*row].key, bantu);
             strcpy((*tabel)[*row].value[0], help);
             (*row)++;
-        } else{
+        } else{                                 //jika ada string bantu yang tersimpan pada key
             while((strcmp((*tabel)[x].value[j], " ") != 0)&&(strcmp((*tabel)[x].value[j], help) != 0)){
-                j++;
+                j++;    //mencari posisi value kosong pada row ke x dari tabel
             }
             strcpy((*tabel)[x].value[j], help);
         }
