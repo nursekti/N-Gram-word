349121977ba582b34e43d91b31de78689a950d71
edoariasena <49637330+edoariasena@users.noreply.github.com>
2020-04-16 02:05:06 +0700

    Update Proses.h

--- Proses.h
+++ Proses.h
@@ -5,8 +5,7 @@
 * Asisten (NIM) : Irfan Tito Kurniawan (18317019)
 * Nama File : Proses.h
 * Deskripsi : Terdiri dari dua fungsi-prosedur yaitu CariWords() dan NGram()
-*             - CariWords() berguna untuk mencari indeks dari key yang sesuai dengan n kata yang dimasukkan. 
-*               N kata tersebut terlebih dahulu diambil dari susunan kata dari keluaran yang sedang di generate.
+*             - CariWords() berguna untuk mencari indeks dari key yang sesuai 
 *             - NGram() berguna untuk memasukkan kata-kata dari Linked List yang berisi string dari file eksternal,
 *               ke dalam suatu NGram dengan jumlah N kata key yang bergantung pada masukan N di Main.c
 */
@@ -16,7 +15,7 @@ int CariWords(char* nword, tabelngram *tabel, int panjang)
     int i;
     i = 0;
     while (i <= panjang){                           //While loop sampai akhir array (indeks terakhir)
-        if (strcmp(nword, tabel[i].key) == 0){      //Membandingkan nword yang dicari dengan key per indeks
+        if (strcmp(nword, tabel[i].key) == 0){      //Membandingkan nword yang dicari dengan key per indeksnya
             return i;                               //Mengembalikan nilai indeks jika ditemukan kecocokan string
         } else{
             i++;                                    //Jika tidak ditemukan, lanjut mencari di indeks selanjutnya
@@ -35,7 +34,7 @@ void NGram(node *S, int count, tabelngram **tabel, int n, int *row){
         (*tabel)[i].key = (char*)malloc((n*50)*sizeof(char));
         for(j = 0; j < 20; j++){
             (*tabel)[i].value[j] = (char*)malloc(15*sizeof(char));
-            strcpy((*tabel)[i].value[j], " ");                      //menginiasialisasi semua value dengan " "
+            strcpy((*tabel)[i].value[j], " ");                      //menginisialisasi semua value dengan " "
         }
     }
     temp = (node*)malloc(sizeof(node));
